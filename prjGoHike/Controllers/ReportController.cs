ced0ed30224aff37693c6d41688083ef6f87f17d
JASON <jason092092@gmail.com>
2026-08-14 17:02:33 +0800

    新增ReportController跟view

---
+++ prjGoHike/Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prjGoHike.Models;
+
+namespace prjGoHike.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly GoHikeDataContext _context;
+
+        public ReportController(GoHikeDataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var reports = _context.Reports.ToList();
+            return View(reports);
+        }
+    }
+}
