0a9b0caec5a99406dafc403b946bfe15f85bc6da
JASON <jason092092@gmail.com>
2026-08-14 21:59:12 +0800

    完成檢舉管理功能

--- prjGoHike/Models/Report.cs
+++ prjGoHike/Models/Report.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace prjGoHike.Models;
 
@@ -11,6 +12,7 @@ public partial class Report
 
     public int ArticleId { get; set; }
 
+    [DisplayName("原因")]
     public string Reason { get; set; } = null!;
 
     public string? Reply { get; set; }
@@ -19,8 +21,10 @@ public partial class Report
 
     public DateTime? ReviewDate { get; set; }
 
+    [DisplayName("狀態")]
     public byte Status { get; set; }
 
+    [DisplayName("日期")]
     public DateTime CreatedDate { get; set; }
 
     public virtual User? Admin { get; set; }
