78ed4f2e4874f39f6a2e772e6350e2276adb6eab
jahona180 <jason20101239@gmail.com>
2026-08-16 00:13:33 +0800

    完成編輯山岳按鈕

--- prjGoHike/Controllers/MountainController.cs
+++ prjGoHike/Controllers/MountainController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Client;
+using NetTopologySuite.Geometries;
 using prjGoHike.Models;
 using prjGoHike.MountainViewModel;
 
@@ -14,7 +15,7 @@ namespace prjGoHike.Controllers
         {
             _db = db;
         }
- 
+
         public IActionResult CreatMountainData()
         {
             // 從資料庫取出所有山岳資料，並轉換成畫面需要的顯示格式
@@ -59,18 +60,63 @@ namespace prjGoHike.Controllers
                 return Json(new { success = false, message = ex.Message });
             }
         }
+        [HttpGet]
+        public IActionResult EditEvent(int id)
+        {
+            
+            var moun = _db.Mountains.FirstOrDefault(m => m.MountainId == id);
+            if (moun != null)
+            {
+                return Json(
+                    new
+                    {
+                        success = true,
+                        data = new
+                        {
+                            mountainId = moun.MountainId,
+                            mountainName = moun.MountainName,
+                            location = moun.Location,
+                            altitude = moun.Altitude,
+                            difficultyLevel = moun.DifficultyLevel,
+                            mountainsPermitRequired = moun.MountainsPermitRequired,
+                            nationalParkPermitRequired = moun.NationalParkPermitRequired
+                        }
+                    });
+            }
+            else
+            {
+                return Json(
+                    new { success = false, message = "找不到資料" }
+                    );
+            }
+        }
+        
+        [HttpPost]
         public IActionResult EditEvent(CMountainVM cMountainVM)
         {
-            GoHikeDataContext db = new GoHikeDataContext();
-            var vm = db.Mountains.FirstOrDefault(m => m.MountainId == cMountainVM.MountainW.MountainId);
-            vm.MountainName = cMountainVM.MountainW.MountainName;
-            vm.Location = cMountainVM.MountainW.Location;
-            vm.Altitude = cMountainVM.MountainW.Altitude;
-            vm.DifficultyLevel = cMountainVM.MountainW.DifficultyLevel;
-            vm.MountainsPermitRequired = cMountainVM.MountainW.MountainsPermitRequired;
-            vm.NationalParkPermitRequired = cMountainVM.MountainW.NationalParkPermitRequired;
-            db.SaveChanges();
-            return Json(new { success = true, message = "修改成功!" });
+            try
+            {
+                
+                var moun = _db.Mountains.FirstOrDefault(m => m.MountainId == cMountainVM.MountainW.MountainId);
+                if (moun != null)
+                {
+                    moun.MountainName = cMountainVM.MountainW.MountainName;
+                    moun.Location = cMountainVM.MountainW.Location;
+                    moun.Altitude = cMountainVM.MountainW.Altitude;
+                    moun.DifficultyLevel = cMountainVM.MountainW.DifficultyLevel;
+                    moun.MountainsPermitRequired = cMountainVM.MountainW.MountainsPermitRequired;
+                    moun.NationalParkPermitRequired = cMountainVM.MountainW.NationalParkPermitRequired;
+
+                    _db.SaveChanges();
+                    return Json(new { success = true, message = "資料修改成功！" });
+                }
+
+                return Json(new { success = false, message = "找不到要修改的資料" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
         }
-    }
+    }   
 }
