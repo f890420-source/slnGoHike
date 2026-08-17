608e268d1fd7aa45d55524082d593ab83634520d
jahona180 <jason20101239@gmail.com>
2026-08-17 13:11:21 +0800

    建立modal視窗

---
+++ prjGoHike/EventDataViewModel/CEventDataVM.cs
@@ -0,0 +1,10 @@
+﻿using prjGoHike.Models;
+
+namespace prjGoHike.EventDataViewModel
+{
+    public class CEventDataVM
+    {
+        public List<CEventDataWarp> cEventDatasList { get; set; } = new List<CEventDataWarp>();
+        public CEventDataWarp cEvent { get; set; } = new CEventDataWarp();
+    }
+}
