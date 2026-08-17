77f96100313e17242465cc38d93f859cf73f1e8c
Calvinco <f890420@gmail.com>
2026-08-17 12:17:16 +0800

    AchievementDto.cs SkillTagDto.cs HikeRecordDto.cs  SuspensionDto.cs

---
+++ prjGoHike/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjGoHike.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+    }
+}
---
+++ prjGoHike/Models/Dtos/SkillTagDto.cs
@@ -0,0 +1,6 @@
+﻿namespace prjGoHike.Models.Dtos
+{
+    public class SkillTagDto
+    {
+    }
+}
