c765461bb320b446dc01feb6ff332458ccf758e1
JASON <jason092092@gmail.com>
2026-09-08 23:23:43 +0800

    新增 post api 新增文章還有創建dto

--- prjGoHike/APIControllers/Forum/ArticleController.cs
+++ prjGoHike/APIControllers/Forum/ArticleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using prjGoHike.APIControllers.Forum;
 using prjGoHike.Models;
 using prjGoHike.Models.Dtos.Forum;
 
@@ -65,6 +66,51 @@ namespace prjGoHike.Controllers
             }
 
             return Ok(article);
+        }
+
+        // POST: api/Articles
+        [HttpPost]
+        public async Task<ActionResult<ArticleDto>> CreateArticle(CreateArticleDto dto)
+        {
+            var article = new Article
+            {
+                // TODO: 之後改成從登入會員的 Claims 取得 UserId
+                UserId = 15,
+
+                CategoryId = dto.CategoryId,
+                Title = dto.Title,
+                Content = dto.Content,
+
+                CreatedDate = DateTime.Now,
+                UpdateDate = null,
+                Status = 1
+            };
+
+            _context.Articles.Add(article);
+            await _context.SaveChangesAsync();
+
+            // 重新取得建立完成的文章資料
+            var result = await _context.Articles
+                .Where(a => a.ArticleId == article.ArticleId)
+                .Select(a => new ArticleDto
+                {
+                    ArticleId = a.ArticleId,
+                    UserId = a.UserId,
+                    CategoryId = a.CategoryId,
+                    Title = a.Title,
+                    Content = a.Content,
+                    CreatedDate = a.CreatedDate,
+                    UpdateDate = a.UpdateDate,
+                    Status = a.Status,
+                    CategoryName = a.Category.CategoryName
+                })
+                .FirstAsync();
+
+            return CreatedAtAction(
+                nameof(GetArticle),
+                new { id = article.ArticleId },
+                result
+            );
         }
     }
 }
---
+++ prjGoHike/APIControllers/Forum/CreateArticleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace prjGoHike.APIControllers.Forum
+{
+    public class CreateArticleDto
+    {
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+    }
+}
