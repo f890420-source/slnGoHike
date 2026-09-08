9867297ec82061b57bffdfbd2fadd2e9719879dc
Jason <jason092092@gmail.com>
2026-09-08 12:57:21 +0800

    新增DTO跟API controller

---
+++ prjGoHike/Models/Dtos/Forum/ArticleDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace prjGoHike.Models.Dtos.Forum
+{
+    public class ArticleDto
+    {
+        public int ArticleId { get; set; }
+
+        public long UserId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public byte Status { get; set; }
+
+        public string? CategoryName { get; set; }
+    }
+}
+
