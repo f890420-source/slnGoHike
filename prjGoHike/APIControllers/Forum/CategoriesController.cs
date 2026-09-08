be81e7e7ae4f1b82410c922a596c83fa0da278fe
JASON <jason092092@gmail.com>
2026-09-08 22:42:00 +0800

    新增category api跟Dto

---
+++ prjGoHike/APIControllers/Forum/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using prjGoHike.Models;
+using prjGoHike.Models.Dtos.Forum;
+
+namespace prjGoHike.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly GoHikeDataContext _context;
+
+        public CategoriesController(GoHikeDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories()
+        {
+            var categories = await _context.Categories
+                .Select(c => new CategoryDto
+                {
+                    CategoryId = c.CategoryId,
+                    CategoryName = c.CategoryName
+                })
+                .ToListAsync();
+
+            return Ok(categories);
+        }
+    }
+}
---
+++ prjGoHike/Models/Dtos/Forum/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace prjGoHike.Models.Dtos.Forum
+{
+    public class CategoryDto
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
