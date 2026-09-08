857a1e135974021ec61474aacc1baf53743b8b38
Jason <jason092092@gmail.com>
2026-09-08 13:53:58 +0800

    新增article controlle

--- prjGoHike/APIControllers/Forum/ArticleController.cs
+++ prjGoHike/APIControllers/Forum/ArticleController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prjGoHike.Models;
+using prjGoHike.Models.Dtos.Forum;
 
-namespace prjGoHike.APIControllers.Forum
+namespace prjGoHike.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ArticleController : ControllerBase
+    public class ArticlesController : ControllerBase
     {
+        private readonly GoHikeDataContext _context;
+
+        public ArticlesController(GoHikeDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Articles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ArticleDto>>> GetArticles()
+        {
+            var articles = await _context.Articles
+                .Select(a => new ArticleDto
+                {
+                    ArticleId = a.ArticleId,
+                    UserId = a.UserId,
+                    CategoryId = a.CategoryId,
+                    Title = a.Title,
+                    Content = a.Content,
+                    CreatedDate = a.CreatedDate,
+                    UpdateDate = a.UpdateDate,
+                    Status = a.Status,
+                    CategoryName = a.Category.CategoryName
+                })
+                .ToListAsync();
+
+            return Ok(articles);
+        }
     }
 }
