896f3f1ea4f5a1f02abbe1c4dd02c00a744e3fcf
reboot0707 <bootcamp@contact.clam.tw>
2026-09-09 11:25:23 +0800

    feat(GoHikeSafe): add Data Trasfer Objects for API design

---
+++ prjGoHike/DTO/GoHikeSafe/TrailDto.cs
@@ -0,0 +1,30 @@
+﻿using NetTopologySuite.Geometries;
+using System.ComponentModel.DataAnnotations;
+
+namespace prjGoHike.DTO.GoHikeSafe
+{
+    public class TrailDto
+    {
+        [Key]
+        public long TrailId { get; set; }
+
+        public string TrailName { get; set; } = null!;
+
+        public string Region { get; set; } = null!;
+
+        public int DifficultyLevel { get; set; }
+
+        public decimal? DistanceKm { get; set; }
+
+        public decimal? EstimatedHours { get; set; }
+
+        public bool PermitRequired { get; set; }
+
+        public bool GuideRequired { get; set; }
+
+        public string? RegulationNote { get; set; }
+
+        public bool IsPublished { get; set; }
+        public Geometry? TrailPath { get; set; }
+    }
+}
