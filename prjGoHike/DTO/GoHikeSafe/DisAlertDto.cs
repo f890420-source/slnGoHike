f7f2bbe6e11010b33832eb3bcbff9c24ae030aaf
reboot0707 <bootcamp@contact.clam.tw>
2026-09-09 11:25:23 +0800

    feat(GoHikeSafe): add Data Trasfer Objects for API design

---
+++ prjGoHike/DTO/GoHikeSafe/DisAlertDto.cs
@@ -0,0 +1,29 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace prjGoHike.DTO.GoHikeSafe
+{
+    public class DisAlertDto
+    {
+        public long AlertId { get; set; }
+
+        public string AlertType { get; set; } = null!;
+
+        public string AlertTitle { get; set; } = null!;
+
+        public string? AlertDescription { get; set; }
+
+        public byte SeverityLevel { get; set; }
+
+        public DateTime EffectiveFrom { get; set; }
+
+        public DateTime? EffectiveTo { get; set; }
+
+        public Geometry? AffectedArea { get; set; }
+
+        public string? SourceAgency { get; set; }
+
+        public string? SourceUrl { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ prjGoHike/DTO/GoHikeSafe/RiskInDto.cs
@@ -0,0 +1,29 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace prjGoHike.DTO.GoHikeSafe
+{
+    public class RiskInDto
+    {
+        public long RiskIndicatorId { get; set; }
+
+        public string IndicatorName { get; set; } = null!;
+
+        public string IndicatorType { get; set; } = null!;
+
+        public decimal Weight { get; set; }
+
+        public byte RiskLevel { get; set; }
+
+        public string? IndicatorDescription { get; set; }
+
+        public Geometry? SpatialArea { get; set; }
+
+        public DateTime? ValidFrom { get; set; }
+
+        public DateTime? ValidTo { get; set; }
+
+        public string? DataSource { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
