5d6b10a07602a06238fa99676663ecb8b50cfd15
Jason <jason092092@gmail.com>
2026-09-10 10:46:05 +0800

    新增公告controller 跟Dto

---
+++ prjGoHike/APIControllers/Forum/AnnouncementsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prjGoHike.Models;
+using prjGoHike.Models.Dtos.Forum;
+using prjGoHike.Dtos;
+
+namespace prjGoHike.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnnouncementsController : ControllerBase
+    {
+        private readonly GoHikeDataContext _context;
+
+        public AnnouncementsController(
+            GoHikeDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAnnouncements()
+        {
+            var announcements = await _context.Announcements
+                .Where(a => a.Status == 1)
+                .OrderByDescending(a => a.CreatedDate)
+                .Select(a => new AnnouncementDto
+                {
+                    AnnouncementId = a.AnnouncementId,
+                    Title = a.Title,
+                    Content = a.Content,
+                    CreatedDate = a.CreatedDate,
+                    UpdateDate = a.UpdateDate
+                })
+                .ToListAsync();
+
+            return Ok(announcements);
+        }
+    }
+}
---
+++ prjGoHike/Models/Dtos/Forum/AnnouncementDto.cs
@@ -0,0 +1,15 @@
+﻿namespace prjGoHike.Models.Dtos.Forum
+{
+    public class AnnouncementDto
+    {
+        public int AnnouncementId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+    }
+}
