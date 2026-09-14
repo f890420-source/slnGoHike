a1e1a326fc02e927a4ce75efa2f432dfdb6e9f2f
Jason <jason092092@gmail.com>
2026-09-14 10:48:29 +0800

    不要罵人

--- prjGoHike/Services/forum/SensitiveWordService.cs
+++ prjGoHike/Services/forum/SensitiveWordService.cs
@@ -2,16 +2,60 @@
 {
     public class SensitiveWordService
     {
-        // 敏感詞清單
+        #region 敏感詞清單
         private readonly string[] _sensitiveWords =
-        {
-            "白癡",
-            "智障",
-            "媽的",
-            "腦殘",
-            "低能"
-        };
+{
+    // 常見辱罵
+    "白癡",
+    "白痴",
+    "智障",
+    "腦殘",
+    "低能",
+    "弱智",
+    "智缺",
+    "腦袋有洞",
+    "沒腦",
+    "有病",
+    "神經病",
+    "垃圾",
+    "廢物",
+    "人渣",
+    "敗類",
+    "畜生",
+    "禽獸",
+    "狗東西",
+    "王八蛋",
+    "混蛋",
 
+    // 常見髒話
+    "媽的",
+    "他媽的",
+    "他媽",
+    "幹你娘",
+    "幹你媽",
+    "操你媽",
+    "靠北",
+    "靠杯",
+    "靠邀",
+    "機掰",
+    "雞掰",
+    "雞巴",
+    "懶叫",
+    "三小",
+    "啥小",
+    "洨",
+
+    // 英文常見辱罵
+    "fuck",
+    "fucking",
+    "shit",
+    "bitch",
+    "asshole",
+    "idiot",
+    "moron",
+    "stupid"
+};
+        #endregion
         // 判斷文字是否包含敏感詞
         public bool ContainsSensitiveWord(string content)
         {
