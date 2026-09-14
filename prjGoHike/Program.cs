602563488fb4b1ccf9a2b92440a1bba54924b81c
reboot0707 <bootcamp@contact.clam.tw>
2026-09-14 11:15:21 +0800

    feat: 注入 GeoJSON 序列化相關功能.

--- prjGoHike/Program.cs
+++ prjGoHike/Program.cs
@@ -25,7 +25,12 @@ builder.Services.AddLogging(config =>
     config.AddConsole();
     config.AddDebug();
 });
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new NetTopologySuite.IO.Converters.GeoJsonConverterFactory());
+    });
 builder.Services.AddSession();
 var app = builder.Build();
 
