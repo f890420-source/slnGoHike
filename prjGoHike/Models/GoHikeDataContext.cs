e7ba5be8857eb7f06f9e2d24301cd51a8a7a0393
reboot0707 <bootcamp@contact.clam.tw>
2026-09-17 14:37:02 +0800

    refactor: 先行重新命名 Model 相關名稱, 以利減少更新 DB 架構工作量.

--- prjGoHike/Models/GoHikeDataContext.cs
+++ prjGoHike/Models/GoHikeDataContext.cs
@@ -73,7 +73,7 @@ public partial class GoHikeDataContext : DbContext
 
     public virtual DbSet<ReviewApplication> ReviewApplications { get; set; }
 
-    public virtual DbSet<RiskIndicator> RiskIndicators { get; set; }
+    public virtual DbSet<Indicator> RiskIndicators { get; set; }
 
     public virtual DbSet<SkillTag> SkillTags { get; set; }
 
@@ -888,7 +888,7 @@ public partial class GoHikeDataContext : DbContext
                 .HasConstraintName("FK_ReviewApplications_ReviewerUserId");
         });
 
-        modelBuilder.Entity<RiskIndicator>(entity =>
+        modelBuilder.Entity<Indicator>(entity =>
         {
             entity.Property(e => e.DataSource).HasMaxLength(200);
             entity.Property(e => e.IndicatorDescription).HasMaxLength(1500);
